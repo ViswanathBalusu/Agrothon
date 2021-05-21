d07c0c6f735c23b264ee8f6670ad9cd745358a4f
Chandra Kiran Viswanath Balusu <kiran.viswanath2017@vitstudent.ac.in>
2021-05-21 14:13:14 +0530

    v1.2.1 - Fix PYPI Release

--- agrothon/__init__.py
+++ agrothon/__init__.py
@@ -5,12 +5,12 @@
 @Path    :   agrothon/
 @Time    :   2021/05/8
 @Author  :   Chandra Kiran Viswanath Balusu
-@Version :   1.2.0
+@Version :   1.2.1
 @Contact :   ckvbalusu@gmail.com
 @Desc    :   Initialization Module for Agrothon
 """
 
-__VERSION__ = "1.2.0"
+__VERSION__ = "1.2.1"
 
 import logging
 import os
