a8cac499c64194159a7c8347d414b6e084116555
Chandra Kiran Viswanath Balusu <67821176+viswanathbalusu@users.noreply.github.com>
2021-05-23 00:38:20 +0530

    🥱 Fix Container Exit

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 agroserver & # FastAPI API Server
 sleep 5
-caddy reverse-proxy --from :"$API_PORT" --to unix//usr/agrothon.sock & # Caddy Reverse Proxy
+caddy reverse-proxy --from :"$API_PORT" --to unix//usr/agrothon.sock # Caddy Reverse Proxy
